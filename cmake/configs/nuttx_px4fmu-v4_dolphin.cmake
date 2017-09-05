be5a6ec724c327e4096a61fb4da80eac73bf5bd5
alsaibie <alsaibie@gmail.com>
2017-09-05 16:28:31 -0400

    commented out dp_pos_control until developed

--- cmake/configs/nuttx_px4fmu-v4_dolphin.cmake
+++ cmake/configs/nuttx_px4fmu-v4_dolphin.cmake
@@ -120,12 +120,12 @@ set(config_module_list
 	#
 	modules/fw_att_control
 	modules/fw_pos_control_l1
-	modules/gnd_att_control
-	modules/gnd_pos_control
+#	modules/gnd_att_control
+#	modules/gnd_pos_control
 #	modules/mc_att_control
 #	modules/mc_pos_control
 	modules/dp_att_control
-	modules/dp_pos_control
+#	modules/dp_pos_control
 	modules/vtol_att_control
 
 	#
