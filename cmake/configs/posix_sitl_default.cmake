94f8af60cf7a014744cdd60d5d80545753f5bd96
alsaibie <alsaibie@gmail.com>
2018-05-07 10:17:42 -0400

    added dp modules and att test to sitl target

--- cmake/configs/posix_sitl_default.cmake
+++ cmake/configs/posix_sitl_default.cmake
@@ -50,6 +50,7 @@ set(config_module_list
 	lib/controllib/controllib_test
 	modules/mavlink/mavlink_tests
 	modules/mc_pos_control/mc_pos_control_tests
+	modules/dp_att_control/dp_att_control_tests
 	modules/uORB/uORB_tests
 	systemcmds/tests
 
@@ -90,6 +91,8 @@ set(config_module_list
 	modules/gnd_pos_control
 	modules/mc_att_control
 	modules/mc_pos_control
+	modules/dp_att_control
+#	modules/dp_pos_control
 	modules/vtol_att_control
 
 	#
