49c31ac15f7771a4422ed380133b9910bee311b2
alsaibie <alsaibie@gmail.com>
2018-05-07 10:09:52 -0400

    User neutral cor

--- setupsitl.sh
+++ setupsitl.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-#source /home/alsaibie/UWSim/install_isolated/setup.bash
-source /home/alsaibie/catkin_ws/devel/setup.bash
-
+#source $HOME/UWSim/install_isolated/setup.bash
+source $HOME/catkin_ws/devel/setup.bash
 source Tools/setup_gazebo.bash $(pwd) $(pwd)/build/posix_sitl_default
 
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd):$(pwd)/Tools/sitl_gazebo
