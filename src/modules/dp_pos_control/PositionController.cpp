837d08c24f1a4872da0542619e3c47ef4d6af70c
alsaibie <alsaibie@gmail.com>
2018-06-22 18:43:08 -0400

    cleaned up

---
+++ src/modules/dp_pos_control/PositionController.cpp
@@ -0,0 +1,5 @@
+//
+// Created by alsaibie on 5/22/18.
+//
+
+#include "PositionController.hpp"
---
+++ src/modules/dp_pos_control/PositionController.hpp
@@ -0,0 +1,16 @@
+//
+// Created by alsaibie on 5/22/18.
+//
+
+#ifndef PX4_POSITIONCONTROLLER_HPP
+#define PX4_POSITIONCONTROLLER_HPP
+
+
+
+class PositionController {
+
+};
+
+
+
+#endif //PX4_POSITIONCONTROLLER_HPP
